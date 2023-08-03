50b57372cf7201ccf57c1acc1c3cdbb967041d1f
deadaf <gahlyanrohit18@gmail.com>
2023-08-03 11:40:33 +0530

    dump body

--- handler.py
+++ handler.py
@@ -4,6 +4,6 @@ import json
 def generate(event, context):
     request_body = json.loads(event["body"])
 
-    response = {"statusCode": 200, "body": request_body}
+    response = {"statusCode": 200, "body": json.dumps(request_body)}
 
     return response
